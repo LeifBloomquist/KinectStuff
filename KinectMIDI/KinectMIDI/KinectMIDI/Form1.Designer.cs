52d7281844713f15b44644096e719c8a14695cf4
Leif Bloomquist <github@leifbloomquist.net>
2018-07-04 01:02:30 -0400

    Re-added MIDI

--- KinectMIDI/KinectMIDI/KinectMIDI/Form1.Designer.cs
+++ KinectMIDI/KinectMIDI/KinectMIDI/Form1.Designer.cs
@@ -34,13 +34,14 @@
             this.lMIDI = new System.Windows.Forms.Label();
             this.cbSeated = new System.Windows.Forms.CheckBox();
             this.lFrame = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // bStart
             // 
             this.bStart.Font = new System.Drawing.Font("OCR A Extended", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.bStart.ForeColor = System.Drawing.Color.Green;
-            this.bStart.Location = new System.Drawing.Point(366, 79);
+            this.bStart.Location = new System.Drawing.Point(427, 12);
             this.bStart.Name = "bStart";
             this.bStart.Size = new System.Drawing.Size(112, 48);
             this.bStart.TabIndex = 0;
@@ -66,7 +67,7 @@
             this.lDetails.ForeColor = System.Drawing.Color.White;
             this.lDetails.Location = new System.Drawing.Point(12, 79);
             this.lDetails.Name = "lDetails";
-            this.lDetails.Size = new System.Drawing.Size(335, 537);
+            this.lDetails.Size = new System.Drawing.Size(159, 537);
             this.lDetails.TabIndex = 2;
             this.lDetails.Text = "--";
             // 
@@ -75,9 +76,9 @@
             this.lMIDI.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.lMIDI.Font = new System.Drawing.Font("OCR A Extended", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lMIDI.ForeColor = System.Drawing.Color.White;
-            this.lMIDI.Location = new System.Drawing.Point(621, 198);
+            this.lMIDI.Location = new System.Drawing.Point(184, 79);
             this.lMIDI.Name = "lMIDI";
-            this.lMIDI.Size = new System.Drawing.Size(335, 475);
+            this.lMIDI.Size = new System.Drawing.Size(163, 537);
             this.lMIDI.TabIndex = 3;
             this.lMIDI.Text = "--";
             // 
@@ -105,12 +106,24 @@
             this.lFrame.Text = "--";
             this.lFrame.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("OCR A Extended", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(335, 23);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Player 1 Track/MIDI";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
             this.ClientSize = new System.Drawing.Size(953, 625);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.lFrame);
             this.Controls.Add(this.cbSeated);
             this.Controls.Add(this.lMIDI);
@@ -123,6 +136,7 @@
             this.MinimizeBox = false;
             this.Name = "Form1";
             this.Text = "Schema Factor Kinect MIDI Tracker Maker Festival 2018";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -137,6 +151,7 @@
         private System.Windows.Forms.Label lMIDI;
         private System.Windows.Forms.CheckBox cbSeated;
         private System.Windows.Forms.Label lFrame;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- KinectMIDI/KinectMIDI/KinectMIDI/Form1.cs
+++ KinectMIDI/KinectMIDI/KinectMIDI/Form1.cs
@@ -16,6 +16,7 @@ namespace KinectMIDI
     public partial class Form1 : Form
     {
         public const String FormatString = "+00.00;-00.00";
+        public const String MIDIFormatString = "000";
 
         KinectHelper kinect = new KinectHelper();
         MIDIHandler midi = MIDIHandler.Instance;
@@ -28,66 +29,20 @@ namespace KinectMIDI
         private void bStart_Click(object sender, EventArgs e)
         {
             kinect.Initialize(HandleFrame);
-            kinect.SetSeatedMode(true);
+            kinect.SetSeatedMode(cbSeated.Checked);
             midi.InitializeMIDI("Kinect");
         }
 
         private void HandleFrame(Skeleton[] skeletons, int framenum)
         {
-            int l_x_cc = 0;
-            int l_y_cc = 0;
-            int l_z_cc = 0;
-
-            int r_x_cc = 0; 
-            int r_y_cc = 0; 
-            int r_z_cc = 0;
-
-            int dist_cc = 0;
-
-            
-
             // Extract and convert to Point3D format
-            List<Player3D> players = KinectHelper.SkeletonsToPlayer3D(skeletons);           
-
-            // MIDI Stuff
-
-            /*
-            // Left
-            if (left != null)
-            { 
-                l_x_cc = midi.ValueToMIDI((float)left.X, -0.5f, 0.5f);
-                l_y_cc = midi.ValueToMIDI((float)left.Y, -0.1f, 0.5f);
-                l_z_cc = midi.ValueToMIDI((float)left.Z, 0.9f, 2.4f);
-
-                midi.SendMIDI(ChannelCommand.Controller, 1, 20, l_x_cc);
-                midi.SendMIDI(ChannelCommand.Controller, 1, 21, l_y_cc);
-                midi.SendMIDI(ChannelCommand.Controller, 1, 22, l_z_cc);
-            }
-
-            // Right
-            if (right != null)
-            {
-                r_x_cc = midi.ValueToMIDI((float)right.X, -0.5f, 0.5f);
-                r_y_cc = midi.ValueToMIDI((float)right.Y, -0.1f, 0.5f);
-                r_z_cc = midi.ValueToMIDI((float)right.Z, 0.9f, 2.4f);
-
-                midi.SendMIDI(ChannelCommand.Controller, 2, 20, r_x_cc);
-                midi.SendMIDI(ChannelCommand.Controller, 2, 21, r_y_cc);
-                midi.SendMIDI(ChannelCommand.Controller, 2, 22, r_z_cc);
-            }
-
-            // Distance
-            xxxx
-
-                dist_cc = midi.ValueToMIDI((float)distance, 0, 1f);
-                midi.SendMIDI(ChannelCommand.Controller, 3, 30, dist_cc);
-        
-            */
+            List<Player3D> players = KinectHelper.SkeletonsToPlayer3D(skeletons);
 
             // Update Screen
             UpdateTrackingDetails(players, framenum);
 
-          
+            // MIDI Processing + Updates Screen
+            ProcessMIDI(players);
         }
 
         private void UpdateTrackingDetails(List<Player3D> players, int framenum)
@@ -115,7 +70,7 @@ namespace KinectMIDI
                         // Left
                         if (player.Left != null)
                         {
-                            details += "\n Left:\n";
+                            details += "\nLeft:\n";
 
                             details += "X = " + player.Left.X.ToString(FormatString) + "\n";
                             details += "Y = " + player.Left.Y.ToString(FormatString) + "\n";
@@ -133,7 +88,7 @@ namespace KinectMIDI
                         }
 
                         // Distance
-                        details += "\nDistance: " + player.Distance.ToString("00.##");
+                        details += "\nDistance:\n" + player.Distance.ToString("00.##");
 
                         lDetails.Text = details;
                     }
@@ -145,33 +100,118 @@ namespace KinectMIDI
             }
         }
 
-        /*
+        // MIDI Stuff
 
-                    midi_details += "Left:\n";
+        private void ProcessMIDI(List<Player3D> players)
+        {
+            Player3D[] player_array = players.ToArray();
 
-                    midi_details +=
-                        l_x_cc.ToString() + "\n" +
-                        l_y_cc.ToString() + "\n" +
-                        l_z_cc.ToString() + "\n";
+            int l_x_cc = 0;
+            int l_y_cc = 0;
+            int l_z_cc = 0;
 
-                    midi_details += "\nRight:\n";
+            int r_x_cc = 0;
+            int r_y_cc = 0;
+            int r_z_cc = 0;
 
-                    midi_details +=
-                        r_x_cc.ToString() + "\n" +
-                        r_y_cc.ToString() + "\n" +
-                        r_z_cc.ToString() + "\n";
+            int b_x_cc = 0;
+            int b_y_cc = 0;
+            int b_z_cc = 0;
 
-                    midi_details += "\nDistance: " + dist_cc.ToString();
+            int dist_cc = 0;
 
-                    lMIDI.Text = midi_details;
-                });
+            for (int i=0; i< player_array.Length; i++)
+            {
+                // Body
+                if (player_array[i].Body != null)
+                {
+                    b_x_cc = midi.ValueToMIDI((float)player_array[i].Body.X, -0.5f, 0.5f);
+                    b_y_cc = midi.ValueToMIDI((float)player_array[i].Body.Y, -0.1f, 0.5f);
+                    b_z_cc = midi.ValueToMIDI((float)player_array[i].Body.Z, 0.9f, 2.4f);
+
+                    midi.SendMIDI(ChannelCommand.Controller, i, 20, b_x_cc);
+                    midi.SendMIDI(ChannelCommand.Controller, i, 21, b_y_cc);
+                    midi.SendMIDI(ChannelCommand.Controller, i, 22, b_z_cc);
+                }
+
+                // Left
+                if (player_array[i].Left != null)
+                {
+                    l_x_cc = midi.ValueToMIDI((float)player_array[i].Left.X, -0.5f, 0.5f);
+                    l_y_cc = midi.ValueToMIDI((float)player_array[i].Left.Y, -0.1f, 0.5f);
+                    l_z_cc = midi.ValueToMIDI((float)player_array[i].Left.Z, 0.9f, 2.4f);
+
+                    midi.SendMIDI(ChannelCommand.Controller, i, 30, l_x_cc);
+                    midi.SendMIDI(ChannelCommand.Controller, i, 31, l_y_cc);
+                    midi.SendMIDI(ChannelCommand.Controller, i, 32, l_z_cc);
+                }
+
+                // Right
+                if (player_array[i].Right != null)
+                {
+                    r_x_cc = midi.ValueToMIDI((float)player_array[i].Right.X, -0.5f, 0.5f);
+                    r_y_cc = midi.ValueToMIDI((float)player_array[i].Right.Y, -0.1f, 0.5f);
+                    r_z_cc = midi.ValueToMIDI((float)player_array[i].Right.Z, 0.9f, 2.4f);
+
+                    midi.SendMIDI(ChannelCommand.Controller, i, 40, r_x_cc);
+                    midi.SendMIDI(ChannelCommand.Controller, i, 41, r_y_cc);
+                    midi.SendMIDI(ChannelCommand.Controller, i, 42, r_z_cc);
+                }
+
+                // Distance
+
+                dist_cc = midi.ValueToMIDI((float)player_array[i].Distance, 0f, 1f);
+                midi.SendMIDI(ChannelCommand.Controller, i, 50, dist_cc);
+           
+
+                // TODO - Detect jumps, claps, etc.  *******
+
+
+                // Update Screen
+                string details = "";
+
+                // Body
+                if (player_array[i].Body != null)
+                {
+                    details += "Body:\n";
+
+                    details += "X = " + b_x_cc.ToString(MIDIFormatString) + "\n";
+                    details += "Y = " + b_y_cc.ToString(MIDIFormatString) + "\n";
+                    details += "Z = " + b_z_cc.ToString(MIDIFormatString) + "\n";
+                }
+
+                // Left
+                if (player_array[i].Left != null)
+                {
+                    details += "\nLeft:\n";
+
+                    details += "X = " + l_x_cc.ToString(MIDIFormatString) + "\n";
+                    details += "Y = " + l_y_cc.ToString(MIDIFormatString) + "\n";
+                    details += "Z = " + l_z_cc.ToString(MIDIFormatString) + "\n";
+                }
+
+                // Right
+                if (player_array[i].Right != null)
+                {
+                    details += "\nRight:\n";
+
+                    details += "X = " + r_x_cc.ToString(MIDIFormatString) + "\n";
+                    details += "Y = " + r_y_cc.ToString(MIDIFormatString) + "\n";
+                    details += "Z = " + r_z_cc.ToString(MIDIFormatString) + "\n";
+                }
+
+                // Distance
+                details += "\nDistance:\n" + player_array[i].Distance.ToString(MIDIFormatString);
+                lMIDI.Text = details;
             }
-            
         }
-         * 
-         */
 
         private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
             Application.Exit();
         }
